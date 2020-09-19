cbd90f0c61f318923a3038bd408701e476e73d6f
RexJoush <joush970918@sina.com>
2020-09-19 10:55:54 +0800

    2020.09.19

---
+++ JavaEE/Demo02/src/main/java/com/joush/loginTest/ServletDemo01.java
@@ -0,0 +1,10 @@
+@javax.servlet.annotation.WebServlet("/ServletDemo01")
+public class ServletDemo01 extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+        
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+        this.doPost(request, response);
+    }
+}
